c72bde20e28222e7fd00f0f0cb9374aeb6e56dec
cplan85 <carlos_planchart@live.com>
2022-05-08 15:29:15 +0200

    Fixed hamburger menu transition

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,25 +4,26 @@ const hamburger = document.querySelector(".menu-btn_burger");
 
 const nav = document.querySelector(".nav");
 
-const menuNav = document.querySelector("menu-nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav_item");
 
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if(!showMenu) {
-    hamburger.classList.add('open');
-    nav.classList.add('open');
-    menuNav.classList.add('open');
-    navItems.forEach(item => item.classList.add('open'));
-// ctrl plus d is a good shortcut for selecting multiple items
+  if (!showMenu) {
+    hamburger.classList.add("open");
+    nav.classList.add("open");
+    menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
+    // ctrl plus d is a good shortcut for selecting multiple items
     showMenu = true;
   } else {
-    hamburger.classList.remove('open');
-    nav.classList.remove('open');
-    menuNav.classList.remove('open');
-    navItems.forEach(item => item.classList.remove('open'));
+    hamburger.classList.remove("open");
+    nav.classList.remove("open");
+    menuNav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
 
     showMenu = false;
   }
